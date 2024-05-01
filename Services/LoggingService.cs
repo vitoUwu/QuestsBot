09fa856ed1c48745f32c5293976cb207cc349290
vitoUwu <victorhugomeurerdelgrandi@gmail.com>
2024-04-30 22:57:41 -0400

    feat: add serilog

--- Services/LoggingService.cs
+++ Services/LoggingService.cs
@@ -1,36 +1,33 @@
 ﻿using Discord;
-using Discord.Commands;
+using Serilog;
+using Serilog.Core;
+using Serilog.Events;
 
 namespace QuestsBot.Services
 {
     public class LoggingService
     {
-        public Task LogAsync(LogMessage message)
-        {
-            if (message.Exception is CommandException cmdException)
-            {
-                Console.WriteLine($"[Command/{Colorize(message.Severity)}] {cmdException.Command.Aliases.First()}"
-                    + $" failed to execute in {cmdException.Context.Channel}.");
-                Console.WriteLine(cmdException);
-            }
-            else
-                Console.WriteLine($"[General/{Colorize(message.Severity)}] {message}");
+        private readonly Logger logger = new LoggerConfiguration()
+            .MinimumLevel.Verbose()
+            .Enrich.FromLogContext()
+            .WriteTo.Console()
+            .CreateLogger();
 
-            return Task.CompletedTask;
-        }
-
-        static private string Colorize(LogSeverity severity)
+        public async Task LogAsync(LogMessage message)
         {
-            return severity switch
+            var severity = message.Severity switch
             {
-                LogSeverity.Critical => $"\u001b[31;1mCritical\u001b[0m",
-                LogSeverity.Error => $"\u001b[31mError\u001b[0m",
-                LogSeverity.Warning => $"\u001b[33mWarning\u001b[0m",
-                LogSeverity.Info => $"\u001b[32mInfo\u001b[0m",
-                LogSeverity.Verbose => $"\u001b[34mVerbose\u001b[0m",
-                LogSeverity.Debug => $"\u001b[34mDebug\u001b[0m",
-                _ => $"\u001b[37m{severity}\u001b[0m"
+                LogSeverity.Critical => LogEventLevel.Fatal,
+                LogSeverity.Error => LogEventLevel.Error,
+                LogSeverity.Warning => LogEventLevel.Warning,
+                LogSeverity.Info => LogEventLevel.Information,
+                LogSeverity.Verbose => LogEventLevel.Verbose,
+                LogSeverity.Debug => LogEventLevel.Debug,
+                _ => LogEventLevel.Information
             };
+
+            logger.Write(severity, message.Exception, "[{Source}] {Message}", message.Source, message.Message);
+            await Task.CompletedTask;
         }
     }
 }
